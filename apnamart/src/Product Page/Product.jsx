89074497c62e984d16759b733a9eb9ee2823aaa8
VivBelwal <04vivek10belwal.com>
2022-11-09 14:06:52 +0530

    added file structure of Product Page

---
+++ apnamart/src/Product Page/Product.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Routebar from './Routebar';
+import Sidebar from './Sidebar';
+const Product = () => {
+  return (
+    <div>
+      <Routebar />
+      <Sidebar />
+    </div>
+  )
+}
+
+export default Product
