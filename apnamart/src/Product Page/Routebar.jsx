f86d2af53c162207d47e6ef2d1b547b9e4871a7e
VivBelwal <04vivek10belwal.com>
2022-11-10 00:00:28 +0530

    added work

--- apnamart/src/Product Page/Routebar.jsx
+++ apnamart/src/Product Page/Routebar.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import {Box, Hide, Text} from "@chakra-ui/react"
-import route from './Product.module.css'
+import styles from './Product.module.css'
 const Routebar = () => {
   return (
     <Box >
       <Hide breakpoint='(max-width: 789px)'>
-    <Box className= {route.Routebar}>This text hides at the "md" value screen width and smaller.</Box>
+    <Box className= {styles.routeBar}>This text hides at the "md" value screen width and smaller.</Box>
   </Hide>
 
 
