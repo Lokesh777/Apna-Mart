8aa032c69787eda9f1a7d9592d391e5bc1067a03
AYUSH KUMAR <ayushpratapsingh235@gmail.com>
2022-11-12 18:55:06 +0530

    routes added

--- apnamart/src/App.js
+++ apnamart/src/App.js
@@ -2,11 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 import Product from './Product Page/Product';
 import CartPage from './Cart Page/CartPage';
+import MainHome from './HomePage/MainHome';
+import { Route, Routes } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <Product />
-      <CartPage />
+      <Routes>
+        <Route path="/" element={<MainHome />} />
+{   /*     <Route path="/about" element="about" />
+        <Route path="/contacts" element="contacts" />
+        <Route path="/login" element="login" />
+        <Route path="/user" element={<PrivateRoute></PrivateRoute>} />
+  <Route path="/users/:user_id" element={<PrivateRoute></PrivateRoute>} /> */}
+      </Routes>
     </div>
   );
 }
