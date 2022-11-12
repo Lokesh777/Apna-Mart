1260aa3d3661966d5ceca66a872f9404f5451b02
VivBelwal <04vivek10belwal.com>
2022-11-12 20:03:08 +0530

    aded

--- apnamart/src/App.js
+++ apnamart/src/App.js
@@ -2,13 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import Product from './Product Page/Product.jsx';
 import CartPage from './Cart Page/CartPage';
-import MainHome from './HomePage/MainHome';
+
 import { Route, Routes } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<MainHome />} />
+        {/* <Route path="/" element={<MainHome />} /> */}
+        <Route path="/product" element={<Product/>} />
+        <Route path="/cart" element={<CartPage/>} />
 {   /*     <Route path="/about" element="about" />
         <Route path="/contacts" element="contacts" />
         <Route path="/login" element="login" />
--- apnamart/src/index.js
+++ apnamart/src/index.js
@@ -4,15 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ChakraProvider } from '@chakra-ui/react';
+import {BrowserRouter} from "react-router-dom"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <ChakraProvider>
+ <BrowserRouter>
+  <ChakraProvider>
     <App />
     </ChakraProvider>
+ </BrowserRouter>
+   
   
-  </React.StrictMode>
+ 
 );
 
 // If you want to start measuring performance in your app, pass a function
